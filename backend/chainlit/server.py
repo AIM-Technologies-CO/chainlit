9027edb4a51b83d21c5901b828dce1f28ca8bac6
Ahmed Rashwan <ahmed.t.rashwan@gmail.com>
2023-10-26 11:28:39 +0300

    Update server.py for watermark change

--- backend/chainlit/server.py
+++ backend/chainlit/server.py
@@ -601,7 +601,7 @@ async def get_logo(theme: Optional[Theme] = Query(Theme.light)):
 
     for path in [
         os.path.join(APP_ROOT, "public", f"logo_{theme_value}.*"),
-        os.path.join(build_dir, "assets", f"logo_{theme_value}*.*"),
+        os.path.join(APP_ROOT, "public", f"watermark_logo_{theme_value}*.*"),
     ]:
         files = glob.glob(path)
 
